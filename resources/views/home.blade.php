6f48288aa0098d74ae8743db336049c2f3293376
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2024-05-10 07:25:07 +0530

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,24 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+  <!-- start page title -->
+  <div class="row">
+    <div class="col-12">
+        <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+            <h4 class="mb-sm-0">Starter</h4>
 
-                    {{ __('You are logged in!') }}
-                </div>
+            <div class="page-title-right">
+                <ol class="breadcrumb m-0">
+                    <li class="breadcrumb-item"><a href="javascript: void(0);">Pages</a></li>
+                    <li class="breadcrumb-item active">Starter</li>
+                </ol>
             </div>
+
         </div>
     </div>
 </div>
+<!-- end page title -->
+
+
 @endsection
